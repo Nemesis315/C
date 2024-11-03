a4d3ffb70cf3f1e5f49d359c41f491b911b03a92
Nemesis315 <cmatteo.conte@gmail.com>
2024-11-03 13:29:14 +0000

    update

--- D-Istruzioni_decisionali_e_iterative/Esercizio_F_if.c
+++ D-Istruzioni_decisionali_e_iterative/Esercizio_F_if.c
@@ -3,6 +3,12 @@ Autore: Matteo Conte
 Classe: 3H
 Data:03/11/2024
 
+Calcolo del voto finale con bonus Scrivere un programma che calcoli il voto finale di uno studente in base alla sua media e al numero di crediti ottenuti durante il semestre. Le regole sono le seguenti:
+Se la media è maggiore o uguale a 28 e lo studente ha accumulato più di 24 crediti, aggiungere un bonus di 2 punti al voto finale.
+Se la media è tra 25 e 27 inclusi, e lo studente ha accumulato tra 18 e 24 crediti, aggiungere un bonus di 1 punto.
+Se la media è inferiore a 25 o i crediti sono meno di 18, il voto finale rimane invariato. Il programma deve visualizzare la media iniziale, il numero di crediti e il voto finale (compreso il bonus, se applicabile).
+
+
 Input: media, numero di crediti dello studente
 Elaborazione: voto finale
 Output: voto finale con bonus
@@ -16,10 +22,10 @@ int main()
     int md, cr, voto_finale;
 
     //Acquisizione della media e dei crediti da tastiera
-    printf("Inserisci la media dello studente: ");
+    printf("Inserire la media dello studente: ");
     scanf("%d", &md);
     printf("\n");
-    printf("Inserisci i crediti ottenuti durante il semestre dello studente: ");
+    printf("Inserire i crediti ottenuti dallo studente durante il semestre: ");
     scanf("%d", &cr);
     printf("\n");
 
@@ -39,7 +45,7 @@ int main()
     }
     else
     {
-        if (md >= 25 && md <=27 && cr >= 18 && cr <=24)
+        if ((md >= 25 && md <=27) && (cr >= 18 && cr <=24))
         {
             voto_finale = md+1;
             printf("Media iniziale: %d", md);
