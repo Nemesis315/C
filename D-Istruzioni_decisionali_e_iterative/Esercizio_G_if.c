cd722250e72b31fb3aebc0e8ff3951036f307eac
Nemesis315 <cmatteo.conte@gmail.com>
2024-11-04 09:59:53 +0000

    update

---
+++ D-Istruzioni_decisionali_e_iterative/Esercizio_G_if.c
@@ -0,0 +1,25 @@
+/*
+Autore: Matteo Conte
+Classe: 3H
+Data:04/11/2024
+
+letto un carattere da tastiera visualizzare a schermo il carattere e i corrispondenti codici ascii in decimale e esadecimale
+
+Input: carattere da tastiera 
+Elaborazione: codice ascii in decimale è esadecimale
+Output: stampare a schermo il valore corrispondente in codice ascii in decimale è esadecimale
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    // dichiarazione delle variabile
+    int carattere;
+
+    printf("Inserire il carattere: ");
+    sscanf("%d", &carattere);
+    printf("\n");
+
+    
+}
