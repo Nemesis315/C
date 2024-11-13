7eefb7e8c3e3836521128e5fe8b4db1512ed66de
Nemesis315 <cmatteo.conte@gmail.com>
2024-11-13 10:59:51 +0000

    update

---
+++ D-Istruzioni_decisionali_e_iterative/ES02-Esercizi_cicli/Esercizio_B_ciclo.c
@@ -0,0 +1,17 @@
+/*
+Auore: Matteo Conte
+Classe: 3H
+Data:12/11/2024
+
+Input: 2 numeri interi
+Elaborazione: numeri tra i 2 numeri iniziali
+Output: numeri compresi tra i 2 iniziali
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int num1,num2;
+    
+}
