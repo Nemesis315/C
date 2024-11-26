ccfb47770fb3d34eb560c4131a5ed639a8d051d0
Nemesis315 <cmatteo.conte@gmail.com>
2024-11-26 15:46:14 +0000

    update

---
+++ D-Istruzioni_decisionali_e_iterative/ES03 - Cicli for/Es_B_ciclo_for.c
@@ -0,0 +1,29 @@
+/*
+Auore: Matteo Conte
+Classe: 3H
+Data:26/11/2024
+
+Esercizio: Scrivere un programma che visualizzi tutte le coppie ordinate di numeri naturali la cui somma è 15.
+
+Elaborazione: divisori di 15 
+Output: stampo a schermo tutti i divisori di 15
+*/
+
+#include <stdio.h>
+
+int main() {
+    int Div1,Div2;
+    
+    // Visualizza tutte le coppie di numeri naturali la cui somma è 15
+    printf("Le coppie ordinate di numeri naturali la cui somma è 15 sono:\n");
+    
+    for (Div1 = 0; Div1 <= 15; Div1++) {
+        for (Div2 = 0; Div2 <= 15; Div2++) {
+            if (Div1 + Div2 == 15) {
+                printf("(%d, %d)\n", Div1, Div2);
+            }
+        }
+    }
+    
+    return 0;
+}
