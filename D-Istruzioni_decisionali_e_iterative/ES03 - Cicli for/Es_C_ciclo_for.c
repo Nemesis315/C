1406de918a3da79599559c510b9c94ca8a175ee4
Nemesis315 <cmatteo.conte@gmail.com>
2024-11-27 10:41:17 +0000

    update

---
+++ D-Istruzioni_decisionali_e_iterative/ES03 - Cicli for/Es_C_ciclo_for.c
@@ -0,0 +1,13 @@
+/*
+Auore: Matteo Conte
+Classe: 3H
+Data:27/11/2024
+
+Esercizio: Scrivere un programma che letto un numero intero da tastiera, stampi tutti  i suoi divisori (stampa nel ciclo!) 
+e quindi visualizzi una sola volta (fuori dal ciclo) se è un numero primo o non lo è.
+
+Input: numero intero
+Elaborazione: divisori del numero e se e un numero primo
+Output: stampo a schermo tutti i divisori e numero primo
+*/
+
