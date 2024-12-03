a440449aa94e8eaaca604681b591527c165e6e8f
Nemesis315 <cmatteo.conte@gmail.com>
2024-12-03 14:42:43 +0000

    update

--- Esercizi_di_laboratorio/D-Istruzioni_decisionali_e_iterative/ES03 - Cicli for/Es_C_ciclo_for.c
+++ Esercizi_di_laboratorio/D-Istruzioni_decisionali_e_iterative/ES03 - Cicli for/Es_C_ciclo_for.c
@@ -15,43 +15,5 @@ Output: stampo a schermo tutti i divisori e numero primo
 
 int main() 
 {
-    int n, i;
-
-    // Leggi un numero intero da tastiera
-    printf("Inserisci un numero intero: ");
-    scanf("%d", &n);
-
-    // Stampiamo i divisori del numero
-    printf("I divisori di %d sono: ", n);
-    for(i = 1; i <= n; i++) 
-    {
-        if (n % i == 0) 
-        {
-            printf("%d ", i);
-        }
-    }
-    printf("\n");
-
-    // Verifica se il numero è primo
-    int isPrime = 1; // Assume che il numero sia primo
-    for(i = 2; i <= n / 2; i++) 
-    {
-        if (n % i == 0) 
-        {
-            isPrime = 0; // Se il numero è divisibile per un altro numero oltre a 1 e se stesso, non è primo
-            break;
-        }
-    }
-
-    // Stampa se il numero è primo o no
-    if (isPrime && n > 1) 
-    {
-        printf("%d è un numero primo.\n", n);
-    } 
-    else 
-    {
-        printf("%d non è un numero primo.\n", n);
-    }
-
-    return 0;
+    
 }
