8993f06ce551040ef29afd21c1c5b3d3727a081a
Nemesis315 <cmatteo.conte@gmail.com>
2024-12-13 10:06:10 +0000

    update

---
+++ Esercizi_di_laboratorio/D-Istruzioni_decisionali_e_iterative/ES03 - Cicli for/Es_F_ciclo_for.c
@@ -0,0 +1,12 @@
+/*
+Auore: Matteo Conte
+Classe: 3H
+Data:11/12/2024
+
+Esercizio: Scrivere un programma che stampi tutti i numeri primi compresi tra 1 e 100. Un numero è primo quando ha come divisore uno e sé stesso, 
+Quindi è primo ciascun numero naturale maggiore di 1 che sia divisibile solamente per 1 e per sé stesso.
+
+Input: numeri compresi tra 1 e 100
+Elaborazione: calcolo dei numeri primi 
+Output: stampo a schermo tutti i numeri primi
+*/
