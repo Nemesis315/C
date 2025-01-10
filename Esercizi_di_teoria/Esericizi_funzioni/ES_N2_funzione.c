a019f124f24d7a73271c439f9661073a73db15ca
Nemesis315 <cmatteo.conte@gmail.com>
2025-01-10 10:57:39 +0000

    update

---
+++ Esercizi_di_teoria/Esericizi_funzioni/ES_N2_funzione.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int MCD (int,int);
+
+int main()
+{
+    int Num1,Num2;
+    int Ris;
+
+    printf("Inserire un numero intero: ");
+    scanf("%d", &Num1);
+    printf("Inserire un numero intero: ");
+    scanf("%d", &Num2);
+
+    Ris = MCD (Num1,Num2);
+
+    printf("L'MCD tra %d e %d e: %d", Num1,Num2,Ris);
+
+    return 0;
+}
+
+int MCD (int x, int y)
+{
+    
+}
