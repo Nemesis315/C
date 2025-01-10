075f10aac301e8a8c826121d2b51b3d3c0046617
Nemesis315 <cmatteo.conte@gmail.com>
2025-01-10 10:47:00 +0000

    update

---
+++ Esercizi_di_teoria/Esericizi_funzioni/ES_N1_funzione.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+float Media (int,int);
+
+int main()
+{
+    int a,b;
+    float Ris;
+
+    printf("Inserire un numero intero: ");
+    scanf("%d", &a);
+    printf("Inserire un numero intero: ");
+    scanf("%d", &b);
+
+    Ris = Media(a,b);
+
+    printf("La media tra %d e %d e: %.1f", a,b,Ris);
+    printf("\n");
+
+    return 0;
+}
+
+float Media(int x, int y)
+{
+    float c;
+    c = (float)(x+y)/2;
+    return c;
+}
