8bc626b29de6f3e7c281c9dd3aec8fcf57958646
Nemesis315 <cmatteo.conte@gmail.com>
2025-01-17 10:31:12 +0000

    update

---
+++ Esercizi_di_teoria/Esericizi_funzioni/ES_MCD_Semplifica.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int MCD (int,int);
+void Semplifica (int x, int y, int z);
+
+int main()
+{
+    int Num1,Num2;
+    int Ris;
+
+    printf("Inserire un numero interno per il numeratore: ");
+    scanf("%d", &Num1);
+    printf("Inserire un numero intero per il denominatore: ");
+    scanf("%d", &Num2);
+
+    Ris = MCD (Num1,Num2);
+
+    printf("L'MCD tra %d e %d e: %d", Num1,Num2,Ris);
+
+    return 0;
+}
+
+int MCD (int x, int y)
+{
+    while (x != y)
+    {
+        if (x > y)
+        {
+            x = x - y;
+        }
+        else
+        {
+            y = y -x;
+        }
+    }
+    
+    return x;
+}
+void Semplifica (int x, int y, int z)
+{
+    x = x / z;
+    
+    y = y / z;
+
+    printf("La frazione semplificata e %d/%d", x,y);
+}
