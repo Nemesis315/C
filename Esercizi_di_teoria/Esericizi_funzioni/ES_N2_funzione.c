7acf0e4d3e874cef3bb26677336b61d13a8a8b41
Nemesis315 <cmatteo.conte@gmail.com>
2025-01-17 10:25:58 +0000

    update

--- Esercizi_di_teoria/Esericizi_funzioni/ES_N2_funzione.c
+++ Esercizi_di_teoria/Esericizi_funzioni/ES_N2_funzione.c
@@ -20,5 +20,17 @@ int main()
 
 int MCD (int x, int y)
 {
+    while (x != y)
+    {
+        if (x > y)
+        {
+            x = x - y;
+        }
+        else
+        {
+            y = y -x;
+        }
+    }
     
+    return x;
 }
