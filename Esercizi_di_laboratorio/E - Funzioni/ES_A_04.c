148a6030505eb262b78dd627a5d2ae21866551c5
Nemesis315 <cmatteo.conte@gmail.com>
2025-01-29 15:21:52 +0000

    update

--- Esercizi_di_laboratorio/E - Funzioni/ES_A_04.c
+++ Esercizi_di_laboratorio/E - Funzioni/ES_A_04.c
@@ -13,11 +13,31 @@ Aggiungere alla funzione menu le chiamate alle tre precedenti funzioni per testa
 int area_rettangolo (int base,int altezza);
 void Dnum(int x);
 int MCD (int x ,int y);
-void menu();
 
 int main()
 {
+    int Numero;
+    printf("\n");
 
+    switch (Numero)
+    {
+    case 0:
+        int Num1,Num2,Area;
+
+        printf("Inserire un numero intero come base del rettangolo: ");
+        scanf("%d", &Num1);
+        printf("Inserire un numero intero come altezza del rettangolo ");
+        scanf("%d", &Num2);
+        
+        Area= area_rettangolo(Num1,Num2);
+
+        printf("L'area del rettangolo con base %d e altezza %d e: %d", Num1,Num2,Area);
+        
+        break;
+    
+    default:
+        break;
+    }
 }
 void Dnum(int x)     
 {
