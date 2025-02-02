9c4fbe3080027f260fa77d7cb6044a9f148c8f70
Nemesis315 <cmatteo.conte@gmail.com>
2025-02-02 10:26:41 +0000

    update

--- TPSI/Saluti2.0.sh
+++ TPSI/Saluti2.0.sh
@@ -10,10 +10,9 @@ clear  #Cancello tutto lo schermo prima di eseguire lo script
 
 read -p "Inserisci il tuo nome: " nome     #Chiedo all'utente di inserire il suo nome e poi lo leggo
 
-read -p "Inserire ora corrente" ora minuti   #Chiedo all'utente di inserire l'ora corrente
-                        #Leggo l'ora inserita dall'utente
+read -p "Inserire ora e minuti correnti: " ora minuti   #Chiedo all'utente di inserire l'ora e i minuti correnti poi li leggo
 
 #echo "Inserire minuti correnti: "   #Chiedo all'utente di inseire i minuti correnti
 #read minuti                         #Leggo i minuti inseriti dall'utente
 
-echo "Ciao $nome sono le $ora$minuti"      #Stampo a schermo un messaggio che saluta l'utente e che gli dice che ore sono
+echo "Ciao $nome sono le $ora.$minuti"      #Stampo a schermo un messaggio che saluta l'utente e che gli dice che ore sono
