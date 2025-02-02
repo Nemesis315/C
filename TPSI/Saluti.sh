01ca102d8baf5833755737fdbca9844c849a417f
Nemesis315 <cmatteo.conte@gmail.com>
2025-02-02 10:59:07 +0000

    update

--- TPSI/Saluti.sh
+++ TPSI/Saluti.sh
@@ -12,7 +12,5 @@ read -p "Inserisci il tuo nome: " nome     #Chiedo all'utente di inserire il suo
 
 read -p "Inserire ora e minuti correnti: " ora minuti   #Chiedo all'utente di inserire l'ora e i minuti correnti poi li leggo
 
-#echo "Inserire minuti correnti: "   #Chiedo all'utente di inseire i minuti correnti
-#read minuti                         #Leggo i minuti inseriti dall'utente
+echo "Ciao $nome sono le $ora:$minuti"      #Stampo a schermo un messaggio che saluta l'utente e che gli dice che ore sono
 
-echo "Ciao $nome sono le $ora.$minuti"      #Stampo a schermo un messaggio che saluta l'utente e che gli dice che ore sono
--- TPSI/Saluti2.0.sh
+++ TPSI/Saluti2.0.sh
@@ -7,12 +7,15 @@ ora=""      #Variabile ora
 minuti=""       #Variabile minuti
 
 clear  #Cancello tutto lo schermo prima di eseguire lo script
+echo -n "Inserire il nome: "
 
-read -p "Inserisci il tuo nome: " nome     #Chiedo all'utente di inserire il suo nome e poi lo leggo
+if [ "$nome" = char ]
+then
+    
+
+else
+fi
 
 read -p "Inserire ora e minuti correnti: " ora minuti   #Chiedo all'utente di inserire l'ora e i minuti correnti poi li leggo
-
-#echo "Inserire minuti correnti: "   #Chiedo all'utente di inseire i minuti correnti
-#read minuti                         #Leggo i minuti inseriti dall'utente
 
 echo "Ciao $nome sono le $ora.$minuti"      #Stampo a schermo un messaggio che saluta l'utente e che gli dice che ore sono
