72ac33e05c2a8e319ce1307f5194fdf786162cd9
Nemesis315 <cmatteo.conte@gmail.com>
2025-02-02 11:24:51 +0000

    update

--- TPSI/Saluti2.0.sh
+++ TPSI/Saluti2.0.sh
@@ -8,14 +8,39 @@ minuti=""       #Variabile minuti
 
 clear  #Cancello tutto lo schermo prima di eseguire lo script
 echo -n "Inserire il nome: "
+read nome
 
-if [ "$nome" = char ]
+if [ "$nome"  ]
 then
-    
+    echo -n "Inserire l'ora corrente: "
+    read ora
+
+
+    echo -n "Inserire minuti correnti: "
+        read minuti
+
+    if [ "$ora" -ge 1 && "$ora" -le 24 ] && [ "$minuti" -ge 1 && "$minuti" -le 60 ]
+    then
+        echo -n "Inserire minuti correnti: "
+        read minuti
+
+
+
+    else
+        echo "L'ora inseriti non e corretta"
+
+        echo -n "Reinserire l'ora: "
+        read ora
+
+    fi
 
 else
-fi
 
-read -p "Inserire ora e minuti correnti: " ora minuti   #Chiedo all'utente di inserire l'ora e i minuti correnti poi li leggo
+    echo "Il nome inserito e sbaglio"
+
+    echo -n "Reinserire il nome: "
+    read nome
+
+fi
 
 echo "Ciao $nome sono le $ora.$minuti"      #Stampo a schermo un messaggio che saluta l'utente e che gli dice che ore sono
