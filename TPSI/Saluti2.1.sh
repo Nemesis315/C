bf97f2642942a2aecea500994cd01324c0b74213
Nemesis315 <cmatteo.conte@gmail.com>
2025-02-04 07:18:44 +0000

    update

---
+++ TPSI/Saluti2.1.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+nome=""     #Variabile nome
+
+ora=""      #Variabile ora
+
+minuti=""       #Variabile minuti
+
+clear  #Cancello tutto lo schermo prima di eseguire lo script
+echo -n "Inserire il nome: "
+read nome
+
+if [ -z "$nome" ]
+then
+    echo "Attenzione! Inserire un nome"
+
+    echo -n "Inserire il nome: "
+    read nome
+
+else
+    echo -n "Inserire l'ora corrente: "
+    read ora
+
+    echo -n "Inserire minuti correnti: "
+    read minuti
+
+    if [ $ora -ge 1 -a $ora -le 24 ] 
+    then
+
+        if [ "$minuti" -ge 1 -a "$minuti" -le 60 ]
+        then
+
+            echo -n "Inserire minuti correnti: "
+            read minuti
+        else
+
+            echo
+        fi
+
+
+    else
+        echo "L'ora inseriti non e corretta"
+
+        echo -n "Reinserire l'ora: "
+        read ora
+
+    fi
+fi
+
+echo "Ciao $nome sono le $ora.$minuti"      #Stampo a schermo un messaggio che saluta l'utente e che gli dice che ore sono
