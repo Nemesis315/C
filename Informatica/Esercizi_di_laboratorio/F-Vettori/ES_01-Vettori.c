f312788493504bbc0f966072fe52be51029ad26f
Nemesis315 <cmatteo.conte@gmail.com>
2025-03-10 09:58:47 +0000

    update

---
+++ Informatica/Esercizi_di_laboratorio/F-Vettori/ES_01-Vettori.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define N 10
+
+
+
+
+int main()
+{
+    int vettore1[N] = {1,2,3,4,5,6,7,8,9,10};
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("I voti che hai inserito sono: %d", vettore1[i]);
+        printf("\n");
+    }
+    
+
+    return 0;
+}
+
+
+
+
