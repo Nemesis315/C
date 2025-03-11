9fc6423f48e237daa82e822fbfc37a437c87b469
Nemesis315 <cmatteo.conte@gmail.com>
2025-03-11 14:27:38 +0000

    update

--- Informatica/Esercizi_di_laboratorio/F-Vettori/ES_a3-Vettori.c
+++ Informatica/Esercizi_di_laboratorio/F-Vettori/ES_a3-Vettori.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+int Ricerca(int v[], int num);
+
 #define N 15
 
 int main()
@@ -20,6 +22,35 @@ int main()
     printf("Inserire il numero da cercare all'interno dell'arrey (Compreso tra 1 e 20): ");
     scanf("%d", &Numero_da_trovare);
 
-    
+
+    int trovato = Ricerca(Vettore, Numero_da_trovare);
+
+    if(trovato == -1)
+    {
+        printf("Il numero %d da lei cercato non e presente all'interno dell'arrey", Numero_da_trovare);
+    }
+    else
+    {
+        printf("Il numero cercato si trova nella casella di incide %d", trovato);
+    }
+
+    return 0;
+}
+
+
+int Ricerca(int v[], int num)
+{
+    for (int i = 0; i < N; i++)
+    {
+        if (v[i] == num)
+        {
+            return num;
+        }
+        
+    }
+
+    return -1;
     
 }
+
+ 
