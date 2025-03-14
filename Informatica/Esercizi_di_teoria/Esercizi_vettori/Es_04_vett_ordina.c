6df7229dd9deb10dcb2d136627c905a041f34b77
Nemesis315 <cmatteo.conte@gmail.com>
2025-03-14 10:16:59 +0000

    update

---
+++ Informatica/Esercizi_di_teoria/Esercizi_vettori/Es_04_vett_ordina.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 10
+
+void ordina(int v[]);
+
+
+void stampa_array(int v[]);
+
+
+int main()
+{
+    int vettore[N];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < N; i++)
+    {
+        vettore[i] = rand() % 11;
+    }
+
+    ordina(vettore);
+
+    stampa_array(vettore);
+
+
+
+}
+
+
+void ordina(int v[])
+{
+    int z;
+
+    for (int i = 0; i < N - 1 ; i++)
+    {
+        for (int j = i +1; j < N; j++)
+        {
+            if (v[i] > v[j])
+            {
+                z = v[i];
+
+                v[i] = v[j];
+
+                v[j] = z;
+            }
+            
+            
+        }
+        
+    }
+    
+}
+
+
+void stampa_array(int v[])
+{
+    for (int i = 0; i < N; i++)
+    {
+        printf("I numeri inseriti casualmente sono: %d", v[i]);
+        printf("\n");
+    }
+    
+}
