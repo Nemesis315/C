70507c221eb22640b409bcc673690ba9f2e2a059
Nemesis315 <cmatteo.conte@gmail.com>
2025-03-25 07:27:18 +0000

    update

--- TPSI/ES_D/Es_D.sh
+++ TPSI/ES_D/Es_D.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Controlla se l'argomento è stato fornito
-if [ -z "$1" ]; then
-  echo "Devi fornire il numero di righe come argomento."
-  exit 1
-fi
-
-# Assegna l'argomento a una variabile
-n=$1
+read -p "Inserisci il numero di righe: " num
+num=""
 
 # Ciclo per stampare il triangolo
-for ((i=1; i<=n; i++))
+for ((i=1; i<=num; i++))
 do
   # Stampa gli asterischi
   for ((j=1; j<=i; j++))
