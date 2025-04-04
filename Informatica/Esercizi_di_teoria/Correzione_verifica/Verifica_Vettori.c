79593e931f50fdd20e443b455915f0d980298ae8
Nemesis315 <cmatteo.conte@gmail.com>
2025-04-04 09:59:52 +0000

    update

--- Informatica/Esercizi_di_teoria/Correzione_verifica/Verifica_Vettori.c
+++ Informatica/Esercizi_di_teoria/Correzione_verifica/Verifica_Vettori.c
@@ -1,23 +1,65 @@
 #include <stdio.h>
+#include <limits.h>
 
 void carica(int v[], int x);
 
+void NuovoInserisci(int v[], int x);
+
+void stampa_array(int v[]);
+
 #define N 100
+
+#define F 10
 
 int main()
 {
     int Vettore[N];
-    int Vettore1[N];
+    int Vettore1[F];
     int Vettore2[N];
     int Vettore3[N];
 
     int num;
 
-    printf("Inserire un numero maggiore di 0 (intero)");
-    scanf("%d", &num);    
+    //printf("Inserire un numero maggiore di 0 (intero): ");
+    //scanf("%d", &num); 
+    
+    //carica(Vettore,num);
+
+    //stampa_array(Vettore);
+
+    NuovoInserisci(Vettore1,1);
+
+    stampa_array(Vettore1);
     
 
-    srand(time(NULL));
+    return 0;
+}
 
 
+void carica(int v[], int x)
+{
+    for (int i = 0; i < N; i++)
+    {
+        v[i] = x++;
+    }
+}
+
+void NuovoInserisci(int v[], int x)
+{
+    v[0] = x;
+
+    for (int i = 1; i < F; i++)
+    {
+        v[i] = x*=2;
+    }
+}
+
+void stampa_array(int v[])
+{
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d", v[i]);
+        printf("\n");
+    }
+    
 }
