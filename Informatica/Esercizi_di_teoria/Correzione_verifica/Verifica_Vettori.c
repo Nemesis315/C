df2feaf82f641cd0d6186dcab1f4a262ee05685f
Nemesis315 <cmatteo.conte@gmail.com>
2025-04-04 09:40:02 +0000

    update

---
+++ Informatica/Esercizi_di_teoria/Correzione_verifica/Verifica_Vettori.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void carica(int v[], int x);
+
+#define N 100
+
+int main()
+{
+    int Vettore[N];
+    int Vettore1[N];
+    int Vettore2[N];
+    int Vettore3[N];
+
+    int num;
+
+    printf("Inserire un numero maggiore di 0 (intero)");
+    scanf("%d", &num);    
+    
+
+    srand(time(NULL));
+
+
+}
