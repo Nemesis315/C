315c6cd52199b776ca990b3c87c29bb5f93715ba
Nemesis315 <cmatteo.conte@gmail.com>
2025-04-14 09:51:23 +0000

    update

---
+++ Informatica/Esercizi_di_teoria/Correzione_verifica/Es_autovalutazione.c
@@ -0,0 +1,81 @@
+#include "stdio.h"
+int main()
+{
+    /** int a=10,b=20, somma=0;
+     * 
+     *printf("%d + %d = %d",a,b,somma);
+    */
+
+
+    /**int a=10;
+     * printf("valore variabile = a");
+     * 
+    */
+
+
+
+    /**int a=7,b=2;
+     *float media;
+     *media=a+b/2;
+     *printf("%.2f",media);
+    */
+
+
+
+
+    /**int num1=5,num2=2;
+     *float quoziente, quoto;
+     *quoziente=(float)num1/num2;
+     *quoto=num1/num2;
+     *printf("%.2f %.2f",quoziente,quoto);
+    */
+
+
+
+    /**int num1=15,num2=222;
+     *if(num1=num2)
+     *printf("uguali");
+     *else
+     *printf("diversi");
+    */
+
+
+
+    /**int num=5;
+     *switch (num)
+     *{ case 2: printf("ciao");
+     *case 5: printf("buongiorno");
+     *default: printf("buonasera");
+     *}
+    */
+
+
+
+    //int num=5;
+    //for (int i=5; i>=5; i--)
+    //{ printf("%d\t",i);
+    //}
+
+
+    //int num=3,i=7;
+    //while (i>num)
+    //{ printf("%d\t%d\t",i, num);
+    //}
+
+
+
+    //int num=7,i=4;
+    //while (i<=num)
+    //{ printf("%d\t%d\t",i, num);
+    //i++;
+    //num--;
+    //}
+
+
+
+    //int num=7,i=4;
+    //for(i=9;i>=0; i-=3)
+    //{ printf("%d\t",i);
+    //}
+
+}
--- Informatica/Esercizi_di_teoria/Esercizi_vettori/Es_04_vett_ordina.c
+++ Informatica/Esercizi_di_teoria/Esercizi_vettori/Es_04_vett_ordina.c
@@ -9,6 +9,8 @@ void ordina(int v[]);
 
 void stampa_array(int v[]);
 
+int ricerca_bin(int v[], int first, int last, int el);
+
 
 int main()
 {
@@ -25,6 +27,24 @@ int main()
 
     stampa_array(vettore);
 
+    int r, el;
+
+    printf("Inserire l'elemento da cercare (0-10): ");
+    scanf("%d", &el);
+
+    r = ricerca_bin(vettore, 0, N-1, el);
+
+    if (r == -1)
+    {
+        printf("L'elemento da lei cercato non è presente all'interno del vettore");
+        printf("\n");
+
+    }
+    else
+    {
+        printf("L'elemento da lei cercato e %d", r);
+        printf("\n");
+    }
 
     return 0;
 }
@@ -62,5 +82,41 @@ void stampa_array(int v[])
         printf("I numeri inseriti casualmente sono: %d", v[i]);
         printf("\n");
     }
+    
+}
+
+int ricerca_bin(int v[], int first, int last, int el)
+{
+    int med;
+
+    if (first > last)
+    {
+        return -1;
+    }
+    else
+    {
+        med = (first + last)/2;
+
+        if (el = v[med])
+        {
+            return med;
+        }
+        else
+        {
+            if (el > v[med])
+            {
+                return ricerca_bin(v, med+1, last, el);
+            }
+            else
+            {
+                return ricerca_bin(v, first, med - 1, el);
+            }
+            
+            
+        }
+        
+        
+    }
+    
     
 }
