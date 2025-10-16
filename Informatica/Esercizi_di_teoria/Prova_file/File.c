62baa9f2f7c8547e08051b97b02919bbccb19225
Nemesis315 <cmatteo.conte@gmail.com>
2025-10-16 07:38:30 +0000

    update

---
+++ Informatica/Esercizi_di_teoria/Prova_file/File.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    char nomeFile[] = { "/workspaces/C/Informatica/Esercizi_di_teoria/Prova_file/file.txt" };
+
+    int err;
+
+    char c;
+
+    FILE *puntaFile;
+
+    err = fopen_s(&puntaFile, nomeFile, "r");
+
+    if (err == 0)
+    {
+        while (!feof(puntaFile))
+        {
+            c = fgetc(puntaFile);
+
+            printf("%c", c);
+
+        }
+        
+        err = fclose(puntaFile);
+    }
+    else
+    {
+        printf("Il file non puo essere aperto");
+
+        printf("\n");
+
+        system("pause");
+    }
+    
+}
