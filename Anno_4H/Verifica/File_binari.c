2638018419f17421de6a3fba2324e11f4d9ff9d1
Nemesis315 <cmatteo.conte@gmail.com>
2025-12-01 12:09:20 +0000

    update

---
+++ Anno_4H/Verifica/File_binari.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#define N 60
+
+void scriviFile (char nomefile[], int k);
+
+struct 
+{
+    char cognome[N];
+
+    int giorno;
+
+    int mese;
+
+    int anno;
+
+    int voti[8];
+
+} typedef studente;
+
+
+int main()
+{
+    int x;
+    srand(time(NULL));
+
+    char nomeFile[N] = { "prova.dat" };
+
+    printf("Inserisci quanti record vuoi scrivere nel file: ");
+    scanf("%d", &x);
+
+    scriviFile(nomeFile, x);
+}
+
+
+
+void scriviFile (char nomefile[], int k)
+{
+    FILE *Fp;
+
+    studente buffer;
+
+    int x = 3;
+
+    Fp = fopen(nomefile, "wb");
+
+    for (int i = 0; i < k; i++)
+    {
+        printf("Inserire il proprio cognome: ");
+        scanf("%s", buffer.cognome);
+
+        for (int j = 0; j < 8; j++)
+        {
+            buffer.voti[j] = rand() % 10 + 1;
+        }
+
+        printf("Inserire il giorno di nascita: ");
+        scanf("%d", buffer.giorno);
+
+        printf("Inserire l'anno di nascita: ");
+        scanf("%d", buffer.anno);
+
+        buffer.mese = x;
+
+        
+    }
+    
+    fclose(Fp);
+}
+
