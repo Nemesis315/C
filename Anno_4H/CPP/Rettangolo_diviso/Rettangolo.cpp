19c48dfa03044ec5eead0777f091f796457a633c
Nemesis315 <cmatteo.conte@gmail.com>
2026-01-19 12:56:34 +0000

    update

--- Anno_4H/CPP/Rettangolo_diviso/Rettangolo.cpp
+++ Anno_4H/CPP/Rettangolo_diviso/Rettangolo.cpp
@@ -3,48 +3,38 @@
 
 using namespace std;
 
-class Rettangolo
+Rettangolo :: Rettangolo(double base, double altezza)
 {
-    private:
-        double base;
-        double altezza;
-    
-    public:
-
-        Rettangolo :: Rettangolo(double base, double altezza)
-        {
-            this->base = base;
-
-            this->altezza = altezza;
-        }
+    this->base = base;
+    this->altezza = altezza;
+}
         
-        void Rettangolo :: setBase(double base)
-        {
-            this->base = base;
-        }
+void Rettangolo :: setBase(double base)
+{
+    this->base = base;
+}
 
-        void Rettangolo :: setAltezza(double altezza)
-        {
-            this->altezza = altezza;
-        }
+void Rettangolo :: setAltezza(double altezza)
+{
+    this->altezza = altezza;
+}
 
-        double Rettangolo :: getBase()
-        {
-            return base;
-        }
+double Rettangolo :: getBase()
+{
+    return base;
+}
 
-        double Rettangolo :: getAltezza()
-        {
-            return altezza;
-        }
+double Rettangolo :: getAltezza()
+{
+    return altezza;
+}
 
-        double Rettangolo :: calcolaArea()
-        {
-            return base * altezza;
-        }
+double Rettangolo :: calcolaArea()
+{
+    return base * altezza;
+}
 
-        double Rettangolo :: calcolaPerimentro()
-        {
-            return 2 * (base + altezza);
-        }
-};
+double Rettangolo :: calcolaPerimentro()
+{
+    return 2 * (base + altezza);
+}
