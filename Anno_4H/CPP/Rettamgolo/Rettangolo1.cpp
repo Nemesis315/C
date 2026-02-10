ee1f78c2cb4959f18a77ed289e603ab633606128
Nemesis315 <cmatteo.conte@gmail.com>
2026-02-10 10:39:02 +0000

    Implemtenazione della classe rettangolo1 .h .cpp e il relativo main.

---
+++ Anno_4H/CPP/Rettamgolo/Rettangolo1.cpp
@@ -0,0 +1,41 @@
+#include "Rettangolo1_h"
+#include <iostream>
+
+using namespace std;
+
+Rettangolo1_h::Rettangolo1_h(int base, int altezza)
+{
+    this->base = base;
+    this->altezza = altezza;
+}
+
+void Rettangolo1_h::setBase(int base)
+{
+    this->base = base;
+}
+
+void Rettangolo1_h::setAltezza(int altezza)
+{
+    this->altezza = altezza;
+}
+
+int Rettangolo1_h::getBase()
+{
+    return base;
+}
+
+int Rettangolo1_h::getAltezza()
+{
+    return altezza;
+}
+
+int Rettangolo1_h::area()
+{
+    return base * altezza;
+}
+
+int Rettangolo1_h::perimetro()
+{
+    return 2 * (base + altezza);
+}
+
---
+++ Anno_4H/CPP/Rettamgolo/main1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Rettangolo1_h"
+#include "Rettangolo1.cpp"
+using namespace std;
+
+int main()
+{
+    Rettangolo1_h* rettangolo1 = new Rettangolo1_h(5, 3);
+    cout << "Base: " << rettangolo1->getBase() << endl;
+    cout << "Altezza: " << rettangolo1->getAltezza() << endl;
+    cout << "Area: " << rettangolo1->area() << endl;
+    cout << "Perimetro: " << rettangolo1->perimetro() << endl;
+
+    delete rettangolo1;
+    return 0;
+}
