76c3a39b00850e129455be5cf88895a2601d7f97
Nemesis315 <cmatteo.conte@gmail.com>
2026-02-10 10:48:41 +0000

    Modifiche ai file main1.cpp e Rettangolo1_h aggiunta di parametri opzionali

--- Anno_4H/CPP/Rettangolo1/main1.cpp
+++ Anno_4H/CPP/Rettangolo1/main1.cpp
@@ -5,12 +5,25 @@ using namespace std;
 
 int main()
 {
-    Rettangolo1_h* rettangolo1 = new Rettangolo1_h(25, 10);
+    Rettangolo1_h* rettangolo1 = new Rettangolo1_h();
+    Rettangolo1_h* rettangolo2 = new Rettangolo1_h(25, 10);
+
+    //Rettangolo 1
+    cout << "Rettangolo 1" << endl;
     cout << "Base: " << rettangolo1->getBase() << endl;
     cout << "Altezza: " << rettangolo1->getAltezza() << endl;
     cout << "Area: " << rettangolo1->area() << endl;
     cout << "Perimetro: " << rettangolo1->perimetro() << endl;
 
+    //Rettangolo 2
+    cout << "Rettangolo 2" << endl;
+    cout << "Base: " << rettangolo2->getBase() << endl;
+    cout << "Altezza: " << rettangolo2->getAltezza() << endl;
+    cout << "Area: " << rettangolo2->area() << endl;
+    cout << "Perimetro: " << rettangolo2->perimetro() << endl;
+
+
     delete rettangolo1;
+    delete rettangolo2;
     return 0;
 }
