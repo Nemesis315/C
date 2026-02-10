3ee263469639184dc2b9bd935b223a1b6b9e2fe1
Nemesis315 <cmatteo.conte@gmail.com>
2026-02-10 10:42:29 +0000

    Modifiche main1.cpp

--- Anno_4H/CPP/Rettangolo1/main1.cpp
+++ Anno_4H/CPP/Rettangolo1/main1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    Rettangolo1_h* rettangolo1 = new Rettangolo1_h(5, 3);
+    Rettangolo1_h* rettangolo1 = new Rettangolo1_h(25, 10);
     cout << "Base: " << rettangolo1->getBase() << endl;
     cout << "Altezza: " << rettangolo1->getAltezza() << endl;
     cout << "Area: " << rettangolo1->area() << endl;
