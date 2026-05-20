009cf62bef125a740ce2c71a92d08bc28d3f8daa
Nemesis315 <cmatteo.conte@gmail.com>
2026-05-20 13:14:45 +0000

    update

--- Lavoro_SIS/TCPClient.ino
+++ Lavoro_SIS/TCPClient.ino
@@ -1,37 +1,64 @@
-#include <SPI.h> //Includere la libreria SPI per la comunicazione con il modulo Ethernet DHCPClient
-#include <Ethernet.h> //Include la libreria Ethernet.h per la gestione la connesione Ethernet
+// Include la libreria SPI.h che viene usata da Arduino per dialogare con il chip Ethernet
+#include <SPI.h> 
 
-byte mac[] = {0x90, 0xAE, 0xA2, 0x01, 0x02, 0x03}; //Indirizzo MAC del client in esadecimale
-byte server[] = {192, 168, 1, 147}; //Indirizzo IP del server a cui il client si connetterà
+// Include la libreria Ethernet per gestire la rete e i protocolli TCP/IP
+#include <Ethernet.h> 
 
+// Definisce l'indirizzo fisico MAC della scheda di rete Arduino
+byte mac[] = {0x90, 0xAE, 0xA2, 0x01, 0x02, 0x03}; 
+
+// Definisce l'indirizzo IP del server di destinazione
+byte server[] = {192, 168, 1, 147}; 
+
+// Crea un oggetto EthernetClient chiamata "client"
 EthernetClient client;
 
+// Funzione di inizializzazione, eseguita una sola volta all'avvio di Arduino
 void setup() 
 {
+  // Inizia la comunicazione con il PC a una velocità di 9600 baud
   Serial.begin(9600);
-  Ethernet.begin(mac); //Inizializza la connessione Ethernet con l'indirizzo MAC del Client
+  
+  // Avvia la configurazione di rete passando solo il MAC per richiedere un indirizzo IP automatico
+  Ethernet.begin(mac); 
+  
+  // Stampa sul monitor seriale il testo
   Serial.print("Indirizzo IP del client: ");
+  
+  // Recupera l'indirizzo IP ottenuto e lo stampa a sul monitor seriale
   Serial.println(Ethernet.localIP());
 
+  // Prova a stabilire una connessione TCP con il server indicato sulla porta 1234
   if (client.connect(server, 1234)) 
   {
+    // Se la connessione va a buon fine stampa a schermo il messaggio connesso
     Serial.println("connesso");
   }
 }
 
-
+// Dichiarazione di una variabile n intera inizializzata a 0
 int n = 0;
+
+// Funzione principale che viene eseguita in loop continuo dopo il setup
 void loop() 
 {
+  // Controlla se il client è connesso al server
   if (client.connected()) 
   {
+    // Invia il valore numerico della variabile n al server
     client.write(n);
+    
+    // Ferma l'esecuzione del programma per 1000 millisecondi
     delay(1000);
 
+    // Controlla se n ha raggiunto il valore 100
     if (n == 100) 
     {
+      // Chiude la connessione TCP con il server
       client.stop();  
     }
+    
+    // Incrementa il valore di n di 1
     n++;
   }   
 }
